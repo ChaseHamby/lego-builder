957a27cb325c50b7f5c74c4b243892124849cf2c
Chase Hamby <c.chase.hamby@gmail.com>
2018-11-08 19:23:59 -0600

    dropdown functionality done and finished randomizer

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import 'jquery';
 import 'bootstrap';
 import './index.scss';
 import legoView from './components/LegoCharacter/legoCharacter';
+import otherView from './helpers/events';
 
 const initializeMainView = () => {
   legoView.initializeLegoView();
+  otherView.displayHeads();
+  otherView.displayTorsos();
+  otherView.displayLegs();
 };
 
 initializeMainView();
